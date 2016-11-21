257308527a2ec3bec33319e48ecb24b87b183bef
Matthias Beyer <mail@beyermatthias.de>
2016-11-21 19:50:56 +0100

    Introduce default Notificators

--- libimagnotification/src/notificator.rs
+++ libimagnotification/src/notificator.rs
@@ -17,9 +17,89 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
-
 /// A Notificator provides a function that can be called to notify about a certain object.
 pub trait Notificator<T> {
     fn notify(&self, item: &T);
+}
+
+pub mod default {
+    use std::fmt::Debug;
+    use std::fmt::Display;
+
+    use notify_rust::Notification as RustNotification;
+    use notify_rust::NotificationUrgency;
+
+    use super::Notificator;
+
+    #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+    pub enum Urgency {
+        Low,
+        Normal,
+        High
+    }
+
+    impl Default for Urgency {
+        fn default() -> Urgency {
+            Urgency::Normal
+        }
+    }
+
+    impl Into<NotificationUrgency> for Urgency {
+
+        fn into(self) -> NotificationUrgency {
+            match self {
+                Urgency::Low    => NotificationUrgency::Low,
+                Urgency::Normal => NotificationUrgency::Normal,
+                Urgency::High   => NotificationUrgency::Critical,
+            }
+        }
+
+    }
+
+    #[derive(Debug, Default, Clone)]
+    pub struct Notification {
+        pub timeout: i32,
+        pub message: String,
+        pub summary: String,
+        pub urgency: Urgency,
+    }
+
+    impl<T: Display> Notificator<T> for Notification {
+
+        /// A default implementation for all Types that implement Display
+        fn notify(&self, item: &T) {
+            let mut n = RustNotification::new();
+            n.appname("imag");
+            n.summary(&self.summary);
+            n.urgency(self.urgency.clone().into());
+            n.body(&format!("{}: {}", &self.message, item));
+            let _ = n.finalize().show(); // Ignoring error here
+        }
+
+    }
+
+    #[derive(Debug, Default, Clone)]
+    pub struct DebugNotification(Notification);
+
+    impl From<Notification> for DebugNotification {
+        fn from(n: Notification) -> DebugNotification {
+            DebugNotification(n)
+        }
+    }
+
+    impl<T: Debug> Notificator<T> for DebugNotification {
+
+        /// A default implementation for all Types that implement Display
+        fn notify(&self, item: &T) {
+            let mut n = RustNotification::new();
+            n.appname("imag");
+            n.summary(&self.0.summary);
+            n.urgency(self.0.urgency.clone().into());
+            n.body(&format!("{}: {:?}", &self.0.message, item));
+            let _ = n.finalize().show(); // Ignoring error here
+        }
+
+    }
+
 }
 
